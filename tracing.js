7ca3245b6fbc09c1dfd3f13ecabb940ded4bf057
dehaarte <jahdehaarte@gmail.com>
2025-12-05 19:34:38 -0500

    Added more dependencies, modified the code to use OTLP Exporter.

--- tracing.js
+++ tracing.js
@@ -1,4 +1,6 @@
 // Referred to and referencing the Observsability Assignment PDF's code instructed to create in tracing.js file
+// Referencing the provided documentation in the assingment pdf: https://opentelemetry.io/docs/languages/js/exporters/
+// Referencing: https://opentelemetry.io/docs/languages/js/exporters/#otlp-dependencies
 const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 
@@ -7,9 +9,16 @@ const { NodeSDK } = require('@opentelemetry/sdk-node');
 const {
     getNodeAutoInstrumentations,
 } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+
+const { 
+    OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-http');
+
 const {
-    ConsoleMetricExporter,
+    OTLPMetricExporter,
+} = require('@opentelemetry/exporter-metrics-otlp-http');
+
+const {
     PeriodicExportingMetricReader,
 } = require('@opentelemetry/sdk-metrics');
 
@@ -17,14 +26,22 @@ const { ExpressInstrumentation } = require("opentelemetry-instrumentation-expres
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 
+const traceExporter = new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+});
+
+const metricReader = new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+        url: 'http://localhost:4318/v1/metrics',
+    }),
+});
+
 const sdk = new NodeSDK({
     resource: resourceFromAttributes({
         [ATTR_SERVICE_NAME]: "todo-service"
     }),
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-    }),
+    traceExporter,
+    metricReader,
     instrumentations: [
         getNodeAutoInstrumentations(),
         new ExpressInstrumentation(),
